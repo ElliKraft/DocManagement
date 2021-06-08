0ae91771978484e9992dc68d9739b911324b22d8
ElliKraft <sunnyelya@gmail.com>
2021-06-08 12:35:19 +0200

    Initial commit

---
+++ src/main/java/de/elvirakraft/docmanagement/DocManagementApplication.java
@@ -0,0 +1,13 @@
+package de.elvirakraft.docmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/elvirakraft/docmanagement/DocManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elvirakraft.docmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
